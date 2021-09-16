8fe974cb41b48bf02a62898af0a0c7c7eb68b6dc
ManOnAJetski <55602478+ManOnAJetski@users.noreply.github.com>
2021-09-16 11:38:40 +0100

    Refactor main code and add initialise test (#4)
    * Refactor main code and add initialise test
    
    * Fix CI build
    
    * Move requirements file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
 import pytest
 import sys
 sys.path.append("./soil-moisture-sensor")
+import app
 
 def test_app():
     pass
+
+def test_connection_string_exists_otherwise_error_raised():
+    with pytest.raises(ValueError) as e_info:
+        app.initialise("")
