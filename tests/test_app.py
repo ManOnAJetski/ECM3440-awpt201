e15911118b2401b8c16c1095722012fc6747c145
Austen Tulodziecki <ezio66@hotmail.co.uk>
2021-09-16 11:35:38 +0100

    Fix CI build

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,6 @@ import app
 def test_app():
     pass
 
-def test_connection_string_exists():
+def test_connection_string_exists_otherwise_error_raised():
     with pytest.raises(ValueError) as e_info:
         app.initialise("")
