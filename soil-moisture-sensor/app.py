bfee7000d11d62012bd1264cf0d3df059f535be7
Austen Tulodziecki <ezio66@hotmail.co.uk>
2021-09-16 11:31:38 +0100

    Refactor main code and add initialise test

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -4,21 +4,27 @@ from counterfit_shims_grove.grove_relay import GroveRelay
 import json
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 from counterfit_connection import CounterFitConnection
-CounterFitConnection.init('127.0.0.1', 5000)
 
-connection_string = (
-    'HostName=ECM3440JetskiHub.azure-devices.net;'
-    'DeviceId=soil-moisture-sensor;'
-    'SharedAccessKey=ijkvLn9ZOI7/aw3IyoBnQdxOD7LM5MCC9/vtgmMDF5s=')
+connection_string = ""
+device_client = None
+relay = None
+adc = None
 
-adc = ADC()
-relay = GroveRelay(5)
+def initialise(new_connection_string):
+    if not new_connection_string:
+        raise ValueError('connection string empty!')
+    connection_string = new_connection_string
 
-device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+def connect():
+    CounterFitConnection.init('127.0.0.1', 5000)
+    adc = ADC()
+    relay = GroveRelay(5)
 
-print('Connecting')
-device_client.connect()
-print('Connected')
+    device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+
+    print('Connecting')
+    device_client.connect()
+    print('Connected')
 
 
 def handle_method_request(request):
@@ -32,15 +38,24 @@ def handle_method_request(request):
     method_response = MethodResponse.create_from_method_request(request, 200)
     device_client.send_method_response(method_response)
 
-
-device_client.on_method_request_received = handle_method_request
-
-
-while True:
+def read_sensor():
     soil_moisture = adc.read(0)
     print("Soil moisture:", soil_moisture)
 
     message = Message(json.dumps({'soil_moisture': soil_moisture}))
     device_client.send_message(message)
 
-    time.sleep(10)
+def run():
+    initialise((
+    'HostName=ECM3440JetskiHub.azure-devices.net;'
+    'DeviceId=soil-moisture-sensor;'
+    'SharedAccessKey=ijkvLn9ZOI7/aw3IyoBnQdxOD7LM5MCC9/vtgmMDF5s='))
+    connect()
+    device_client.on_method_request_received = handle_method_request
+
+    while True:
+        read_sensor()
+        time.sleep(10)
+        
+if __name__ == "__main__":
+    run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
 import pytest
 import sys
 sys.path.append("./soil-moisture-sensor")
+import app
 
 def test_app():
     pass
+
+def test_connection_string_exists():
+    with pytest.raises(ValueError) as e_info:
+        app.initialise("")
